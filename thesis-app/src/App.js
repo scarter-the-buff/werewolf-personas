bef8db91854e2ea15d333ba9531bcfdca9ffcc5e
Sean Carter <collisteru@gmail.com>
2025-01-01 19:44:27 -0700

    Working on menu buttons

--- thesis-app/src/App.js
+++ thesis-app/src/App.js
@@ -1,5 +1,6 @@
 import table from "./table_illustration_nobg.png";
 import Textbox from "./Textbox.js";
+import MenuButton from "./MenuButton.js";
 import "./styles.css";
 import { Helmet } from "react-helmet";
 
@@ -15,7 +16,12 @@ function App() {
         <div className="flex items-center  w-full">
           <Textbox />
           <img src={table} className="table w-1/3 p-5 ml-[100px]" alt="logo" />
+          <div id="tlButtonHolder" class="flex flex-col min-h-[80%]">
+            <MenuButton />
+            <MenuButton />
+          </div>
         </div>
+        <div></div>
       </div>
     </>
   );
---
+++ thesis-app/src/MenuButton.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+
+const MenuButton = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  return (
+    <div className="relative inline-block text-left">
+      {/* Button */}
+      <button
+        onClick={toggleMenu}
+        className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+      >
+        Menu
+        <svg
+          className="-mr-1 ml-2 h-5 w-5"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            fillRule="evenodd"
+            d="M5.23 7.21a.75.75 0 011.06.02L10 10.94l3.71-3.71a.75.75 0 111.06 1.06l-4 4a.75.75 0 01-1.06 0l-4-4a.75.75 0 01.02-1.06z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+
+      {/* Dropdown Menu */}
+      {isOpen && (
+        <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div
+            className="py-1"
+            role="menu"
+            aria-orientation="vertical"
+            aria-labelledby="options-menu"
+          >
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+            >
+              Option 1
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+            >
+              Option 2
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+            >
+              Option 3
+            </a>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MenuButton;
