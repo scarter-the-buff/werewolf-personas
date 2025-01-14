fa9c6f8c95a899d0b30411dbaf7e2a285cc89625
Sean Carter <collisteru@gmail.com>
2025-01-14 11:07:15 -0700

    Menu page frontend made

--- thesis-app/src/Menu.js
+++ thesis-app/src/Menu.js
@@ -3,14 +3,52 @@ import { Link } from "react-router-dom";
 
 function Menu() {
   return (
-    <>
-      <div>Hello, it's a menu!</div>
-      <div>
-        <Link to="/" className="text-white mx-2">
-          Home
-        </Link>
-      </div>
-    </>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        backgroundColor: "#333", // Adjust as needed
+        color: "white",
+      }}
+    >
+      <h1
+        style={{ marginBottom: "2rem", fontSize: "8rem", textAlign: "center" }}
+      >
+        Werewolf
+      </h1>
+      <button
+        style={{
+          padding: "1rem 2rem",
+          fontSize: "1.2rem",
+          marginBottom: "1rem",
+          cursor: "pointer",
+          backgroundColor: "#555",
+          color: "white",
+          border: "none",
+          borderRadius: "5px",
+          width: "200px",
+        }}
+      >
+        NEW GAME
+      </button>
+      <button
+        style={{
+          padding: "1rem 2rem",
+          fontSize: "1.2rem",
+          cursor: "pointer",
+          backgroundColor: "#555",
+          color: "white",
+          border: "none",
+          borderRadius: "5px",
+          width: "200px",
+        }}
+      >
+        LOAD GAME
+      </button>
+    </div>
   );
 }
 
